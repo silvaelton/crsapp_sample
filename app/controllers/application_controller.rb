c82fdb5f8183c0d4a7494247863255766ec8589c
Elton Silva <elton.chrls@gmail.com>
2017-11-10 10:18:33 -0200

    email json

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  def json_email
+    @candidates = Candidate.where(status: 0).map(&:email)
+
+    render json: @candidates
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
 
   root 'site/home#index'
+
+  get 'emailxyz', to: 'application#json_email'
   
   mount Site::Engine    => '/site'
   mount Manager::Engine => '/gestao'
