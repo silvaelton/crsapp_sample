5348ad78b4337bff2c466a2c00dc497285ac3b6f
Elton Silva <elton.chrls@gmail.com>
2017-11-10 10:20:55 -0200

    Litagem json

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def json_email
-    @candidates = Candidate.where(status: 0).map(&:email)
+    @candidates = Candidate.where(status: 1).map(&:email)
 
     render json: @candidates
   end
